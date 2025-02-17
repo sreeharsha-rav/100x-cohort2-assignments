b664023fae56db11ef6d812cf9115b364cdacc65
sreeharsha-rav <dev.sreeharsha@gmail.com>
2025-02-17 11:03:59 +0530

    week-10: 2-prisma-simple

--- week-10/2-prisma-simple/src/db/todo.ts
+++ week-10/2-prisma-simple/src/db/todo.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -12,8 +12,20 @@ const prisma = new PrismaClient();
  *  id: number
  * }
  */
-export async function createTodo(userId: number, title: string, description: string) {
-    
+export async function createTodo(
+  userId: number,
+  title: string,
+  description: string
+) {
+  const todo = await prisma.todo.create({
+    data: {
+      userId,
+      title,
+      description,
+      done: false,
+    },
+  });
+  return todo;
 }
 /*
  * mark done as true for this specific todo.
@@ -26,7 +38,15 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+  const todo = await prisma.todo.update({
+    where: {
+      id: todoId,
+    },
+    data: {
+      done: true,
+    },
+  });
+  return todo;
 }
 
 /*
@@ -40,5 +60,10 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+  return todos;
 }
--- week-10/2-prisma-simple/src/db/user.ts
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
 
 /*
  * Should insert into the users table
@@ -12,8 +11,23 @@ const prisma = new PrismaClient();
  *   name: string
  * }
  */
-export async function createUser(username: string, password: string, name: string) {
-    
+export async function createUser(
+  username: string,
+  password: string,
+  name: string
+) {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+        name,
+      },
+    });
+    return user;
+  } catch (err) {
+    throw new Error("Error creating user: " + err);
+  }
 }
 
 /*
@@ -25,5 +39,14 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  } catch (err) {
+    throw new Error("Error fetching user: " + err);
+  }
 }
